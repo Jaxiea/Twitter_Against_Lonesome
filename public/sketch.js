54c70b42358213e0a16319ab4b9ae2bc542491c8
jada <jada.xex@gmail.com>
2021-05-11 09:55:35 +0200

    Added my project

---
+++ public/sketch.js
@@ -0,0 +1,85 @@
+var socket = io.connect(window.location.origin);
+var tweets = [];
+var historyTweets = [];
+var txtPositionY = 30;     //(windowHeight - fontSize)
+var fontSize = 18;
+var currentLength = 12;
+var newTweetSignal = false;
+var error = [];
+var historyTweetsToggle = false;
+
+var no = 0;
+
+
+socket.on('tweet', function (data) {
+  console.log("got tweets!");
+  console.log(data);
+  tweets = data;
+  newTweetSignal = true;
+});
+
+socket.on('search', function (data) {
+  console.log("got history tweets!");
+  if (data != historyTweets){
+  historyTweets = data;
+  }
+});
+
+
+
+function setup() {
+  var cnv = createCanvas(2500, 810);
+  cnv.position(0, 0, 'absolute');
+  cnv.parent('canvas');
+  background(0);
+  loadFont("Noto_Serif_TC/NotoSerifTC-ExtraLight.otf");
+  historyTweetsToggle == false
+}
+
+
+
+function draw(){
+  fill(113, random(115, 255), 249);
+//  stroke(127, 63, 120);
+  
+  //console.log(historyTweets.length);
+  
+  if (historyTweets && historyTweets.length > 0 && historyTweetsToggle == false){
+    console.log("here!");
+    console.log(historyTweets);
+    textSize(fontSize);
+  
+
+    for (var j = 0; j < historyTweets.length; j++){
+      fill(113, random(115, 255), 249);
+      text(historyTweets[j], 10 + map(noise(no), 0, 1, 0, width/25), txtPositionY);
+      txtPositionY += ( fontSize + 4 );
+      no += 0.05;
+    }
+    
+    historyTweetsToggle = true;
+  }
+
+  if (tweets.length > 0 && newTweetSignal == true && tweets.search("https://") == -1){
+    tweets = tweets.replaceAll('\n', ' ');
+    textSize(fontSize);
+
+    let start_po = map(noise(no), 0, 1, 0, width/25);
+    //if (textWidth(tweets) + start_po < (width - 10)){
+      text(tweets, 10 + start_po, txtPositionY);
+    //}
+    //else{
+      //...
+    //}
+    
+
+    txtPositionY += ( fontSize + 4 );
+    newTweetSignal = false;
+    no += 0.05;
+  }
+
+  if (txtPositionY > height) {
+    background(0);
+    txtPositionY = 48;
+  }
+}
---
+++ server.js
@@ -0,0 +1,229 @@
+// Open a realtime stream of Tweets, filtered according to rules
+// https://developer.twitter.com/en/docs/twitter-api/tweets/filtered-stream/quick-start
+
+const needle = require('needle');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socketIo(server);
+
+var historyTweets = [];
+
+// The code below sets the bearer token from your environment variables
+// To set environment variables on macOS or Linux, run the export command below from the terminal:
+// export BEARER_TOKEN='YOUR-TOKEN'
+const token = process.env.TWITTER_BEARER_TOKEN;
+
+const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules';
+const streamURL = 'https://api.twitter.com/2/tweets/search/stream';
+const searchUrl = "https://api.twitter.com/2/tweets/search/recent";
+
+// this sets up two rules - the value is the search terms to match on, and the tag is an identifier that
+// will be applied to the Tweets return to show which rule they matched
+// with a standard project with Basic Access, you can add up to 25 concurrent rules to your stream, and
+// each rule can be up to 512 characters long
+
+// Edit rules as desired below
+const rules = [{
+        'value': '我的 -is:retweet -is:reply -has:hashtags -has:media -has:images -has:videos',
+        'tag': '我的'
+    },
+];
+
+app.use(express.static('public'));
+/*
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');    //waht exactly is sendfile?
+});
+*/
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+  });
+
+io.on('connection', (socket) => {
+  console.log('a user connected');  
+});
+
+
+
+async function getAllRules() {
+
+    const response = await needle('get', rulesURL, {
+        headers: {
+            "authorization": `Bearer ${token}`
+        }
+    })
+
+    if (response.statusCode !== 200) {
+        console.log("Error:", response.statusMessage, response.statusCode)
+        throw new Error(response.body);
+    }
+
+    return (response.body);
+}
+
+async function deleteAllRules(rules) {
+
+    if (!Array.isArray(rules.data)) {
+        return null;
+    }
+
+    const ids = rules.data.map(rule => rule.id);
+
+    const data = {
+        "delete": {
+            "ids": ids
+        }
+    }
+
+    const response = await needle('post', rulesURL, data, {
+        headers: {
+            "content-type": "application/json",
+            "authorization": `Bearer ${token}`
+        }
+    })
+
+    if (response.statusCode !== 200) {
+        throw new Error(response.body);
+    }
+
+    return (response.body);
+
+}
+
+async function setRules() {
+
+    const data = {
+        "add": rules
+    }
+
+    const response = await needle('post', rulesURL, data, {
+        headers: {
+            "content-type": "application/json",
+            "authorization": `Bearer ${token}`
+        }
+    })
+
+    if (response.statusCode !== 201) {
+        throw new Error(response.body);
+    }
+
+    return (response.body);
+
+}
+
+function streamConnect(retryAttempt, socket) {
+
+    const stream = needle.get(streamURL, {
+        headers: {
+            "User-Agent": "v2FilterStreamJS",
+            "Authorization": `Bearer ${token}`
+        },
+        timeout: 20000
+    });
+
+    stream.on('data', data => {
+        try {
+            const json = JSON.parse(data);
+            socket.emit('tweet', json.data.text);
+            console.log(json.data.text);         //printing it on the console works
+            // A successful connection resets retry count.
+            retryAttempt = 0;
+        } catch (e) {
+            if (data.detail === "This stream is currently at the maximum allowed connection limit.") {
+                console.log(data.detail)
+                process.exit(1)
+            } else {
+                // Keep alive signal received. Do nothing.
+            }
+        }
+    }).on('err', error => {
+        if (error.code !== 'ECONNRESET') {
+            console.log(error.code);
+            process.exit(1);
+        } else {
+            // This reconnection logic will attempt to reconnect when a disconnection is detected.
+            // To avoid rate limits, this logic implements exponential backoff, so the wait time
+            // will increase if the client cannot reconnect to the stream.
+            setTimeout(() => {
+                console.warn("A connection error occurred. Reconnecting...")
+                streamConnect(++retryAttempt);
+            }, 2 ** retryAttempt)
+        }
+    });
+
+    return stream;
+
+}
+
+/* ----- From recent_search.js   ------ */
+
+async function getRequest(socket) {
+
+    // Edit query parameters below
+    // specify a search query, and any additional fields that are required
+    // by default, only the Tweet ID and text fields are returned
+    const params = {
+      'query': '我的 -is:retweet -is:reply -has:hashtags -has:media -has:images -has:videos',
+      'max_results': 30,
+    }
+
+    const res = await needle('get', searchUrl, params, {
+        headers: {
+            "User-Agent": "v2RecentSearchJS",
+            "authorization": `Bearer ${token}`
+        }
+    })
+
+    if (res.body) {
+        historyTweets = res.body.data.map(x => x.text);
+        socket.emit('search', historyTweets);
+        return historyTweets;
+    } else {
+        throw new Error('Unsuccessful request');
+    }
+}
+
+
+
+
+(async () => {
+
+    try {
+        // Make request
+        const response = await getRequest(io);
+        console.log(response);
+
+    } catch (e) {
+        console.log(e);
+        process.exit(-1);
+    }
+
+    let currentRules;
+
+    try {
+        
+        // Gets the complete list of rules currently applied to the stream
+        currentRules = await getAllRules();
+
+        // Delete all rules. Comment the line below if you want to keep your existing rules.
+        await deleteAllRules(currentRules);
+
+        // Add rules to the stream. Comment the line below if you don't want to add new rules.
+        await setRules();
+
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+
+    // Listen to the stream.
+    streamConnect(0, io);
+
+
+})();
